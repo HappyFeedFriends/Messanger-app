47b82a4cd18f95e306c6ecb5a212486a9a41f19b
HappyFeedFriends <31066449+HappyFeedFriends@users.noreply.github.com>
2020-09-10 17:33:45 +0500

    delete body parser

--- back/app.js
+++ back/app.js
@@ -4,22 +4,19 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var apiRouter = require('./routes/api');
-var bodyParser = require('body-parser');
 var methodOverride  = require("method-override");
 var app = express();
  
 // app.use(express.static('public'));
 app.use(logger('dev'));
-// app.use(express.json());
-
 app.use(function(req, res, next) {  
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 }); 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(methodOverride())
 app.use(session({
